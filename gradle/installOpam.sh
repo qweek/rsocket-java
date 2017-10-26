00b8aba80f74da49980077bc9ce03327115652f6
Alex <alnovoselov@mail.ru>
2017-10-27 02:50:15 +0300

    install infer

--- gradle/installOpam.sh
+++ gradle/installOpam.sh
@@ -8,7 +8,7 @@ SYS=$(uname -s) # Linux
 OPAM=1.2.2
 OCAML=4.05.0
 FILE=opam-$OPAM-$ARCH-$SYS
-BINDIR=$HOME/usr/local/bin
+BINDIR=$HOME/usr/local/bin/opam
 
 echo "Download Opam file $FILE"
 wget -q "https://github.com/ocaml/opam/releases/download/$OPAM/$FILE"
@@ -17,10 +17,10 @@ echo "Create Bin directory $BINDIR"
 mkdir -p "$BINDIR"
 
 echo "Install Opam $OPAM"
-install -m 755 $FILE $BINDIR/opam
+install -m 755 $FILE $BINDIR
 
-echo "Install Opam $OPAM"
-$BINDIR/opam init --comp "$OCAML"
+# echo "Install Opam $OPAM"
+# $BINDIR/opam init --comp "$OCAML"
 
 echo "Add $BINDIR/opam to PATH"
 $BINDIR/opam config env
