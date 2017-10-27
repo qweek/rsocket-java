d38ac208e22e98f853c057ac7b9bb25ebb255f31
Alex <alnovoselov@mail.ru>
2017-10-28 02:14:23 +0300

    install infer

--- gradle/installOpam.sh
+++ gradle/installOpam.sh
@@ -30,11 +30,32 @@ export PATH=$BINDIR:$PATH                                                   #opa
 
 ###eval `$BINDIR/opam config env`
 
-echo "Config Opam env 2"
+#echo "Config Opam env 2"
 which opam
-which opam >/dev/null 2>&1
-if ! which opam >/dev/null 2>&1; then
-    echo "dependency not found: fuck"
-fi
+#which opam >/dev/null 2>&1
+#if ! which opam >/dev/null 2>&1; then
+#    echo "dependency not found: fuck"
+#fi
 
-echo "Config Opam env 3"
+#echo "Config Opam env 3"
+
+PLATFORM=linux64
+INFER=0.13.0
+IFILE=infer-$PLATFORM-v$INFER
+LIBDIR=$HOME/usr/local/lib
+
+echo "Download Infer file $IFILE"
+wget -q "https://github.com/facebook/infer/releases/download/v$INFER/$IFILE.tar.xz"  -O - | tar -xJf - && cd $IFILE
+
+echo "Compile Infer $INFER"
+./build-infer.sh java
+
+echo "Create Lib directory $LIBDIR"
+mkdir -p "$LIBDIR"
+
+echo "Install Infer prefix $LIBDIR"
+./configure --prefix=$HOME/usr/local && make install # make
+
+echo "Add Infer to PATH"
+export PATH=`pwd`/infer/bin:$PATH
+which infer
