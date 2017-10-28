91cc46c9f00431e157b9b87531e885b4036a32cd
Alex <alnovoselov@mail.ru>
2017-10-28 20:20:04 +0300

    install infer

--- gradle/cleanup.sh
+++ gradle/cleanup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Remove changes detected in cache"
-rm -f  $HOME/.gradle/caches/*/fileHashes/fileHashes.*
-rm -fr $HOME/.gradle/caches/*/plugin-resolution/
-rm -f  $HOME/.gradle/caches/modules-2/modules-2.lock
-rm -f  $HOME/.gradle/caches/user-id.txt.lock
+rm -fv  $HOME/.gradle/caches/*/fileHashes/fileHashes.bin
+rm -fv  $HOME/.gradle/caches/*/fileHashes/fileHashes.lock
+rm -rfv $HOME/.gradle/caches/*/plugin-resolution/
+rm -fv  $HOME/.gradle/caches/modules-2/modules-2.lock
+rm -fv  $HOME/.gradle/caches/user-id.txt.lock
