b642a76e9764da482315a2f6fbb492e321231977
Alex <alnovoselov@mail.ru>
2017-10-28 18:41:57 +0300

    install infer

--- gradle/infer.sh
+++ gradle/infer.sh
@@ -5,54 +5,15 @@
 PLATFORM=linux64
 INFER=0.13.0
 FILE=infer-$PLATFORM-v$INFER
-USRDIR=$HOME/usr/local/lib/infer/infer/bin
 
-if [ -d "$HOME/usr/local/lib/infer" ]; then
-  echo "Dir $HOME/usr/local/lib/infer exists"
-  if [ -d "$HOME/usr/local/lib/infer/infer/bin" ]; then
-    echo "Dir $HOME/usr/local/lib/infer/infer/bin exists"
-    if [ -f "$HOME/usr/local/lib/infer/infer/bin/infer" ]; then
-      echo "File $HOME/usr/local/lib/infer/infer/bin/infer exists"
-    else
-      echo "File $HOME/usr/local/lib/infer/infer/bin/infer not exists"
-    fi
-  else
-    echo "Dir $HOME/usr/local/lib/infer/infer/bin not exists"
-  fi
-  if [ -d "$HOME/usr/local/lib/infer/infer/man" ]; then
-    echo "Dir $HOME/usr/local/lib/infer/infer/man exists"
-  else
-    echo "Dir $HOME/usr/local/lib/infer/infer/man not exists"
-  fi
+if [ -d "$INFER_DIR" ]; then
+  echo "Dir $INFER_DIR exists"
 else
-  echo "Dir $HOME/usr/local/lib/infer not exists"
-fi
-if [ -d "$HOME/usr/local/share/man/man1" ]; then
-  echo "Dir $HOME/usr/local/share/man/man1 exists"
-  if [ -d "$HOME/usr/local/share/man/man1/infer/man" ]; then
-    echo "Dir $HOME/usr/local/share/man/man1/infer/man exists"
-  else
-    echo "Dir $HOME/usr/local/share/man/man1/infer/man not exists"
-  fi
-else
-   echo "Dir $HOME/usr/local/share/man/man1 not exists"
+  echo "Dir $INFER_DIR not exists"
 fi
 
-echo "Download Infer file $FILE"
+echo "Download file $FILE"
 wget -q "https://github.com/facebook/infer/releases/download/v$INFER/$FILE.tar.xz"  -O - | tar -xJf - && cd $FILE
-
-# TODO: move to infer-linux64-v0.13.0 to $HOME/usr/local/bin/infer ?
-# TODO: cache
-# TODO: ./configure --prefix=$HOME/usr/local first
-# TODO: set $HOME/usr/local
-# delete travis
-# which ocaml
-
-#echo "Create Usr directory $USRDIR"
-#mkdir -p "$USRDIR"
-
-#echo "Configure Infer prefix"
-#INFER_CONFIGURE_OPTS="--prefix=$USRDIR"
 
 echo "Compile Infer $INFER"
 ./build-infer.sh java
@@ -61,11 +22,4 @@ echo "Install Infer"
 make install DESTDIR=$HOME
 
 echo "Remove temporary dir $FILE"
-rm -rf $FILE
-
-#echo "Add Infer to PATH"
-#export PATH=`pwd`/infer/bin:$PATH
-#type -a infer
-#echo "Add Infer to PATH 2"
-#export PATH=$USRDIR:$PATH
-# which infer
+rm -rfv $FILE
--- gradle/opam.sh
+++ gradle/opam.sh
@@ -7,33 +7,24 @@ SYS=$(uname -s) # Linux
 OPAM=1.2.2
 OCAML=4.05.0
 FILE=opam-$OPAM-$ARCH-$SYS
-BINDIR=$HOME/usr/local/bin/opam
 
-if [ -d "$HOME/usr/local/bin/opam" ]; then
-  echo "Dir $HOME/usr/local/bin/opam exists"
-  if [ -f "$HOME/usr/local/bin/opam/opam" ]; then
-    echo "File $HOME/usr/local/bin/opam/opam exists"
-  else
-    echo "File $HOME/usr/local/bin/opam/opam not exists"
-  fi
+if [ -d "$OPAM_DIR" ]; then
+  echo "Dir $OPAM_DIR exists"
 else
-  echo "Dir $HOME/usr/local/bin/opam not exists"
+  echo "Dir $OPAM_DIR not exists"
 fi
 
-echo "Download Opam file $FILE"
+echo "Download file $FILE"
 wget -q "https://github.com/ocaml/opam/releases/download/$OPAM/$FILE"
 
-echo "Create Bin directory $BINDIR"
-mkdir -p "$BINDIR"
+echo "Create directory $OPAM_DIR"
+mkdir -p "$OPAM_DIR"
 
 echo "Install Opam $OPAM"
-install -m 755 $FILE $BINDIR/opam
+install -m 755 $FILE $OPAM_DIR/opam
 
 echo "Remove temporary file $FILE"
-rm -f $FILE
+rm -fv $FILE
 
 # echo "Init Ocaml $OCAML"
-# $BINDIR/opam init --compiler "$OCAML" --no-setup
-
-#echo "Add Opam to PATH"
-#export PATH=$BINDIR:$PATH
+# $OPAM_DIR/opam init --compiler "$OCAML" --no-setup
