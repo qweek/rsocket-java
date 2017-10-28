df7b102348a57b8eae79db1603cef25dd77aa870
Alex <alnovoselov@mail.ru>
2017-10-28 20:47:04 +0300

    install infer

--- gradle/cleanup.sh
+++ gradle/cleanup.sh
@@ -6,3 +6,4 @@ rm -fv  $HOME/.gradle/caches/*/fileHashes/fileHashes.lock
 rm -rfv $HOME/.gradle/caches/*/plugin-resolution/
 rm -fv  $HOME/.gradle/caches/modules-2/modules-2.lock
 rm -fv  $HOME/.gradle/caches/user-id.txt.lock
+rm -fv  $HOME/.gradle/caches/user-id.txt
