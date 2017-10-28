1ba0031bf882b18f72a55455762f2f97de91f1ec
Alex <alnovoselov@mail.ru>
2017-10-28 03:03:43 +0300

    install infer

--- gradle/installInfer.sh
+++ gradle/installInfer.sh
@@ -10,14 +10,16 @@ LIBDIR=$HOME/usr/local/lib
 echo "Download Infer file $FILE"
 wget -q "https://github.com/facebook/infer/releases/download/v$INFER/$FILE.tar.xz"  -O - | tar -xJf - && cd $FILE
 
+# move to infer-linux64-v0.13.0 to $HOME/usr/local/bin/infer ?
+
 echo "Compile Infer $INFER"
 ./build-infer.sh java
 
-echo "Create Lib directory $LIBDIR"
-mkdir -p "$LIBDIR"
+#echo "Create Lib directory $LIBDIR"
+#mkdir -p "$LIBDIR"
 
-echo "Install Infer prefix $LIBDIR"
-./configure --prefix=$HOME && make install # make
+#echo "Install Infer prefix $LIBDIR"
+#./configure --prefix=$HOME && make install # make
 
 echo "Add Infer to PATH"
 export PATH=`pwd`/infer/bin:$PATH
--- gradle/installOpam.sh
+++ gradle/installOpam.sh
@@ -9,7 +9,6 @@ OPAM=1.2.2
 OCAML=4.05.0
 FILE=opam-$OPAM-$ARCH-$SYS
 BINDIR=$HOME/usr/local/bin/opam
-##BINDIR=/usr/local/bin
 
 echo "Download Opam file $FILE"
 wget -q "https://github.com/ocaml/opam/releases/download/$OPAM/$FILE"
@@ -20,42 +19,8 @@ mkdir -p "$BINDIR"
 echo "Install Opam $OPAM"
 install -m 755 $FILE $BINDIR/opam
 
-###echo "Init Ocaml $OCAML"
-###$BINDIR/opam init --compiler "$OCAML" --auto-setup                          #no-setup
+# echo "Init Ocaml $OCAML"
+# $BINDIR/opam init --compiler "$OCAML" --no-setup
 
-echo "Config Opam env"
-# $BINDIR/opam config env                                                   #eval
-##$BINDIR/opam config setup --all
-export PATH=$BINDIR:$PATH                                                   #opam?
-
-###eval `$BINDIR/opam config env`
-
-#echo "Config Opam env 2"
-which opam
-#which opam >/dev/null 2>&1
-#if ! which opam >/dev/null 2>&1; then
-#    echo "dependency not found: fuck"
-#fi
-
-#echo "Config Opam env 3"
-
-#PLATFORM=linux64
-#INFER=0.13.0
-#IFILE=infer-$PLATFORM-v$INFER
-#LIBDIR=$HOME/usr/local/lib
-
-#echo "Download Infer file $IFILE"
-#wget -q "https://github.com/facebook/infer/releases/download/v$INFER/$IFILE.tar.xz"  -O - | tar -xJf - && cd $IFILE
-
-#echo "Compile Infer $INFER"
-#./build-infer.sh java
-
-#echo "Create Lib directory $LIBDIR"
-#mkdir -p "$LIBDIR"
-
-#echo "Install Infer prefix $LIBDIR"
-#./configure --prefix=$HOME/usr/local && make install # make
-
-#echo "Add Infer to PATH"
-#export PATH=`pwd`/infer/bin:$PATH
-#which infer
+echo "Add Opam to PATH"
+export PATH=$BINDIR:$PATH
